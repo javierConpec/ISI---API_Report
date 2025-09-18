606820ebd00c961e09b6c49afb31317e13e254f5
Javier ConPec <javierconpec@gmail.com>
2025-09-18 11:03:50 -0500

    all change

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,33 @@
-FROM node:20
+# 1. Imagen base para build
+FROM node:20-alpine AS builder
 
 WORKDIR /app
+
+# Copiamos package.json y package-lock.json
 COPY package*.json ./
-RUN npm install
+
+# Instalamos todas las dependencias incluyendo devDependencies
+RUN npm ci
+
+# Copiamos el código
 COPY . .
 
+# Instalamos TypeScript globalmente y compilamos
+RUN npm install -g typescript
+RUN tsc
+
+# 2. Imagen final para producción
+FROM node:20-alpine
+
+WORKDIR /app
+
+# Copiamos solo los archivos compilados y package.json
+COPY --from=builder /app/dist ./dist
+COPY --from=builder /app/package*.json ./
+
+# Instalamos solo producción
+RUN npm ci --only=production
+
 EXPOSE 3000
-CMD ["node", "index.js"]
+
+CMD ["node", "dist/server.js"]
--- src/services/chartService.ts
+++ src/services/chartService.ts
@@ -1,17 +1,24 @@
-import {getVentasPorFecha,getVentasPorProducto, getChartNozzleData,getFuelPoints} from "../repositories/chartRepository.js";
+import {getVentasPorFecha,getVentasPorProducto, getChartNozzleData,getFuelPoints, getAniosConVentas, getMesesConVentas} from "../repositories/chartRepository.js";
 
-export const ventasPorFechaService = async () => {
-    return await getVentasPorFecha();
+export const ventasPorFechaService = async (ano?: number, mes?: number) => {
+    return await getVentasPorFecha(ano, mes);
+}
+export const ventasPorProductoService = async (ano?: number, mes?: number) => {
+    return await getVentasPorProducto(ano, mes);
 }
 
-export const ventasPorProductoService = async () => {
-    return await getVentasPorProducto();
-}
-
-export const chartNozzleDataService = async (fuelPointId?:number) => {
-    return await getChartNozzleData(fuelPointId);
+export const chartNozzleDataService = async (LogicalNumber?:number, ano?: number, mes?: number) => {
+    return await getChartNozzleData(LogicalNumber,ano, mes);
 }
 
 export const fuelPointsService = async () => {
     return await getFuelPoints();
 }
+
+export const aniosConVentasService = async () => {
+  return await getAniosConVentas();
+};
+
+export const mesesConVentasService = async (ano?: number) => {
+  return await getMesesConVentas(ano);
+};
